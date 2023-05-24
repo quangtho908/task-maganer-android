261033becc9888700d6a1bcf7ea4b2247227f801
Bui Dinh Nguyen <bdnguyen2k2@gmail.com>
2023-05-24 22:06:22 +0700

    coding dialog create workspace

--- app/src/main/java/com/taskmanager/horkrux/Workspace/WorkspacesActivity.java
+++ app/src/main/java/com/taskmanager/horkrux/Workspace/WorkspacesActivity.java
@@ -1,9 +1,13 @@
 package com.taskmanager.horkrux.Workspace;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -29,6 +33,7 @@ import com.taskmanager.horkrux.CommonUtils;
 import com.taskmanager.horkrux.Models.Count;
 import com.taskmanager.horkrux.Models.Users;
 import com.taskmanager.horkrux.R;
+import com.taskmanager.horkrux.databinding.CreateWorkspaceBinding;
 import com.taskmanager.horkrux.databinding.NavHeaderMainBinding;
 import com.taskmanager.horkrux.databinding.WorkspaceListBinding;
 import com.taskmanager.horkrux.ui.home.HomeFragment;
@@ -58,6 +63,32 @@ public class WorkspacesActivity extends AppCompatActivity {
                 finish();
             }
         });
+        binding.addWorkSpace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialogCreateWorkspace();
+            }
+        });
+    }
+
+    private void showDialogCreateWorkspace(){
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.create_workspace);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int screenWidth = displayMetrics.widthPixels;
+        int dialogWidth = (int) (screenWidth * 0.9);
+        int dialogHeight = ViewGroup.LayoutParams.WRAP_CONTENT;
+        Window window = dialog.getWindow();
+        window.setLayout(dialogWidth, dialogHeight);
+        CreateWorkspaceBinding createWorkspaceBinding = CreateWorkspaceBinding.inflate(getLayoutInflater());
+        createWorkspaceBinding.btnCreateWorkspace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        dialog.show();
     }
 
 }
